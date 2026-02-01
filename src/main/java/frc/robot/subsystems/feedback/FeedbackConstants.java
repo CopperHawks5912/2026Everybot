f6e181b859faa643a6823de5e9a448f0f359850a
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2026-01-31 19:59:53 -0500

    init

--- src/main/java/frc/robot/subsystems/feedback/FeedbackConstants.java
+++ src/main/java/frc/robot/subsystems/feedback/FeedbackConstants.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems.feedback;
 
+import edu.wpi.first.wpilibj.util.Color;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean constants. This
  * class should not be used for any other purpose. All constants should be declared globally (i.e. public static). Do
@@ -13,5 +15,44 @@ package frc.robot.subsystems.feedback;
  * constants are needed, to reduce verbosity.
  */
 public final class FeedbackConstants {
+  /** Number of LEDs in the strip */
+  public static final int LEDLength = 60;
+      
+  /** Color when robot is idle/ready (soft blue) */
+  public static final Color IdleColor = new Color(0.0, 0.3, 1.0);
 
+  /** Color for warnings (orange) */
+  public static final Color WarningColor = new Color(1.0, 0.5, 0.0);
+
+  /** Color for errors (red) */
+  public static final Color ErrorColor = Color.kRed;
+      
+  /** Team color - Green */
+  public static final Color TeamGreen = new Color(0.0, 0.8, 0.2);
+
+  /** Team color - Copper */
+  public static final Color TeamCopper = new Color(0.72, 0.45, 0.20);
+      
+  /**
+   * Enum representing different LED display modes
+   */
+  public enum DisplayMode {
+    /** All LEDs off */
+    OFF,
+    
+    /** Robot idle/ready state */
+    IDLE,
+    
+    /** Warning state */
+    WARNING,
+    
+    /** Error state */
+    ERROR,
+    
+    /** Team colors gradient chase */
+    TEAM_COLORS,
+
+    /** Scoring shift indication */
+    SCORING_SHIFT
+  }
 }
