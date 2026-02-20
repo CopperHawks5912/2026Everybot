c5840f5eca68ff061cd9a74fa61e96c435ef5090
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2026-02-19 22:25:04 -0500

    asdf

--- src/main/java/frc/robot/subsystems/drive/DifferentialSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DifferentialSubsystem.java
@@ -323,9 +323,10 @@ public class DifferentialSubsystem extends SubsystemBase {
   // ==================== Internal State Modifiers ====================
 
   /**
-   * Add a vision measurement consumer to the pose estimator with validation and filtering.
-   * This method can be called by the VisionSubsystem whenever a new vision measurement is available,
-   * and it will handle validation and filtering before adding it to the pose estimator.
+   * Add a vision measurement to the pose estimator with validation and filtering.
+   * This method can be called by the VisionSubsystem whenever a new vision 
+   * measurement is available, and it will handle validation and filtering 
+   * before adding it to the pose estimator.
    * @param visionPose The vision pose to add
    * @param timestamp The timestamp of the vision measurement
    * @param stdDevs The standard deviations of the vision measurement
